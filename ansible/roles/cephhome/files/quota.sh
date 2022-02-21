7b6d0f4c36827badc81a4f00984ddecd9fbefb9e
Kristian Mide <fas@mide.dk>
2022-02-21 14:49:35 +0100

    Present users with quota on login

--- ansible/roles/cephhome/files/quota.sh
+++ ansible/roles/cephhome/files/quota.sh
@@ -8,3 +8,9 @@ DIRECTORY=$(getent passwd ${PAM_USER} | cut -d: -f6)
 
 logger "Setting ${SIZE} byte quota for ${PAM_USER} on ${DIRECTORY}"
 setfattr -n ceph.quota.max_bytes -v ${SIZE} ${DIRECTORY}
+
+# numfmt --to=iec-i --suffix=B `getfattr --absolute-names --only-values -n ceph.quota.max_bytes ${DIRECTORY}`
+
+USAGE=$(numfmt --to=iec-i --suffix=B `getfattr --absolute-names --only-values -n ceph.dir.rbytes ${DIRECTORY}`)
+AVAILABLE=$(numfmt --to=iec-i --suffix=B ${SIZE})
+echo "Current quota usage: ${USAGE} / ${AVAILABLE}"
